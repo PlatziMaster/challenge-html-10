0855876e72ff06fe0b70253b9f9776edfe72ccb0
Israel Castro <israel.castro@gmail.com>
2020-08-04 23:35:49 -0500

    [FIX] Header cover

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
         primary: "#484848",
         secondary: "#7e948c",
       },
+      fontSize: {
+        "7xl": "5rem",
+        "8xl": "6rem",
+      },
       opacity: {
         "10": "0.1",
         "20": "0.2",
