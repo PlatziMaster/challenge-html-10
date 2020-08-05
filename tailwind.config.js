f1cfd175fc327d2ce73966a4db14ef828577f893
Israel Castro <israel.castro@gmail.com>
2020-08-04 22:34:25 -0500

    [ADD] Mobile responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#484848",
+        secondary: "#7e948c",
+      },
+      opacity: {
+        "10": "0.1",
+        "20": "0.2",
+      },
+      inset: {
+        "1/2": "50%",
+        "1/5": "20%",
+        "2/5": "40%",
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
